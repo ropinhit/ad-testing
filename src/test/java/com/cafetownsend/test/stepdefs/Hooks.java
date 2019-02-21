e82ddb1fc52f0b204db5e5abe25639db2629a12e
attdang <anh.thi.tu.dang@mgm-tp.com>
2019-02-21 17:04:32 +0700

    add selenium hub

--- src/test/java/com/cafetownsend/test/stepdefs/Hooks.java
+++ src/test/java/com/cafetownsend/test/stepdefs/Hooks.java
@@ -5,14 +5,21 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Hooks {
     public static WebDriver driver;
 
     @Before
-    public void beforeAllScenario() {
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-        driver = new ChromeDriver();
+    public void beforeAllScenario() throws MalformedURLException {
+//        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "http://dee-app2:4444/wd/hub");
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://dee-app2:4444/wd/hub"),cap );
         driver.manage().window().maximize();
     }
 
